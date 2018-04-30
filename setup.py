68ceadc7fe8c135c8f23a410f1db495abdf6044d
marcominutoli <supporto@marcominutoli.it>
2018-04-30 16:42:07 +0200

    Fix overlayText for video slide

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(name='djangocms-complete-slider',
-      version='0.1.8',
+      version='0.1.9',
       description='A complete slider plugin for Django CMS',
       url='http://github.com/b-dev/djangocms-complete-slider',
       author='Marco Minutoli',
